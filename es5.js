ea0c786bbfe6831dc4c03ae64d2c62099e651e6b
Ben <bromy@users.noreply.github.com>
2019-03-11 16:06:21 -0400

    Use var for ES5 example (#1)

--- es5.js
+++ es5.js
@@ -1,7 +1,7 @@
 (function() {
   console.log('Some good ole ES5 JavaScript');
 
-  const es5El = document.createElement('div');
+  var es5El = document.createElement('div');
   es5El.innerHTML = 'ES5 JS';
   document.getElementById('root-es5').appendChild(es5El);
 })();
