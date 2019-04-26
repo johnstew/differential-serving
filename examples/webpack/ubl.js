d0fc76db85982c3df0e5ebf61398098893414d93
Øyvind Smestad <oyvind.smestad@tidal.com>
2019-04-26 14:29:51 +0200

    Client side bundle loading with workarounds for (most) browser bugs
    This uses the approach from https://medium.com/@WebReflection/a-universal-bundle-loader-6d7f3e628f93
    Basically the deferring of 'ubl.js' should ensure browsers with bugs will load that file, but not any more files if ESM is supported.
    I did simplify the script slightly, to only have a ES2017 and a ES5 bundle (not a ES2015 variant).

---
+++ examples/webpack/ubl.js
@@ -0,0 +1,7 @@
+(function(g,d,s){if(!g.ESM){
+    s=d.documentElement;
+    s=s.insertBefore(d.createElement('script'),s.lastChild);
+    s.defer=!0;
+    s.type='text/javascript';
+    s.src='/dist/index.legacy.js';
+  }}(window,document));
